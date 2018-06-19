8c5085e9a3fce81e8c94e4f005c902e3a14e13bc
berialyue <berialyue@gmail.com>
2018-06-19 13:32:34 +0800

    init finished

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill';
+import fastclick from 'fastclick';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+
+fastclick.attach(document.body);
 
 Vue.config.productionTip = false;
 
