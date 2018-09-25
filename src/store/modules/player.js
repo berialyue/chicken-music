02226429e4fbaaecdd9cab0779f2b54d5e21813f
berialyue <berialyue@gmail.com>
2018-09-25 15:34:03 +0800

    add playMode

--- src/store/modules/player.js
+++ src/store/modules/player.js
@@ -1,4 +1,5 @@
 import { playMode } from 'common/js/config'
+import {shuffle} from 'common/js/util'
 
 const player = {
   state: {
@@ -32,12 +33,33 @@ const player = {
   actions: {
     selectPlay({commit, state}, {list, index}) {
       commit('SET_SEQUENCE_LIST', list)
-      commit('SET_PLAYLIST', list)
+      if (state.mode === playMode.random) {
+        let randomList = shuffle(list)
+        commit('SET_PLAYLIST', randomList)
+        index = findIndex(randomList, list[index])
+      } else {
+        commit('SET_PLAYLIST', list)
+      }
       commit('SET_CURRENT_INDEX', index)
+      commit('SET_FULL_SCREEN', true)
+      commit('SET_PLAYING_STATE', true)
+    },
+    randomPlay({commit}, {list}) {
+      commit('SET_PLAY_MODE', playMode.random)
+      commit('SET_SEQUENCE_LIST', list)
+      let randomList = shuffle(list)
+      commit('SET_PLAYLIST', randomList)
+      commit('SET_CURRENT_INDEX', 0)
       commit('SET_FULL_SCREEN', true)
       commit('SET_PLAYING_STATE', true)
     }
   }
+}
+
+function findIndex(list, song) {
+  return list.findIndex((item) => {
+    return item.id === song.id
+  })
 }
 
 export default player
